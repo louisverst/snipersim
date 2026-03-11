a1cd120170b43b74eca83a13e29c8bf73cbe8139
jacolagrange <jaco.lagrange@ugent.be>
2026-03-11 14:57:12 +0100

    dipMap first implementation

--- common/performance_model/performance_models/rob_performance_model/pics/dip.cc
+++ common/performance_model/performance_models/rob_performance_model/pics/dip.cc
@@ -2,20 +2,24 @@
 
 void DipStack::add_fe_stall(SubsecondTime t)
 {
+    std::cout << "Front end stall add " << t << "\n";
     fe_stall += t;
 }
 
 void DipStack::add_be_stall(SubsecondTime t)
 {
+    std::cout << "Back end stall add " << t << "\n";
     be_stall += t;
 }
 
 void DipStack::add_mispred(SubsecondTime t)
 {
+    std::cout << "Misprediction stall add " << t << "\n";
     mispred += t;
 }
 
 void DipStack::add_base(SubsecondTime t)
 {
+    std::cout << "Base add " << t << "\n";
     base += t;
 }
